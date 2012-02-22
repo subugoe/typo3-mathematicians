8b69780314f1054e1fdece675408c3e79096435f
ssp <porst@sub.uni-goettingen.de>
2012-02-22 18:41:04 +0100

    add existing extension

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "mathematicians".
+#
+# Auto generated 29-06-2010 09:18
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'mathematicians',
+	'description' => 'searching Oberwolfach Photo Collection, MacTutor and Genalogy',
+	'category' => 'plugin',
+	'author' => 'muehlhoelzer',
+	'author_email' => 'mmuehlh@sub.uni-goettingen.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"afd2";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"deae";s:14:"ext_tables.php";s:4:"ee42";s:16:"locallang_db.xml";s:4:"4dee";s:19:"doc/wizard_form.dat";s:4:"00a3";s:20:"doc/wizard_form.html";s:4:"7569";s:35:"pi1/class.tx_mathematicians_pi1.php";s:4:"7b9c";s:17:"pi1/locallang.xml";s:4:"3112";s:24:"pi1/static/editorcfg.txt";s:4:"4715";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"d41d";}',
+);
+
+?>
---
+++ ext_tables.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
+
+
+t3lib_extMgm::addPlugin(array('LLL:EXT:mathematicians/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
+
+
+t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","mathematicians");
+
+t3lib_extMgm::addStaticFile($_EXTKEY,'static//', '');
+?>
