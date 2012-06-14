d26591e1c9abfa1bc2ef2e2ddc8e0f20242172c0
ssp <porst@sub.uni-goettingen.de>
2012-06-14 17:23:24 +0200

    update to version 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mathematicians".
 #
-# Auto generated 29-06-2010 09:18
+# Auto generated 14-06-2012 17:23
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'mathematicians',
-	'description' => 'searching Oberwolfach Photo Collection, MacTutor and Genalogy',
+	'description' => 'searching Oberwolfach Photo Collection and Genalogy',
 	'category' => 'plugin',
 	'author' => 'muehlhoelzer',
 	'author_email' => 'mmuehlh@sub.uni-goettingen.de',
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"afd2";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"deae";s:14:"ext_tables.php";s:4:"ee42";s:16:"locallang_db.xml";s:4:"4dee";s:19:"doc/wizard_form.dat";s:4:"00a3";s:20:"doc/wizard_form.html";s:4:"7569";s:35:"pi1/class.tx_mathematicians_pi1.php";s:4:"7b9c";s:17:"pi1/locallang.xml";s:4:"3112";s:24:"pi1/static/editorcfg.txt";s:4:"4715";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"d41d";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"07ba";s:10:"README.txt";s:4:"4513";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"d5d6";s:14:"ext_tables.php";s:4:"c603";s:13:"locallang.xml";s:4:"7630";s:16:"locallang_db.xml";s:4:"4dee";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:35:"pi1/class.tx_mathematicians_pi1.php";s:4:"a53b";s:39:"pi1/class.tx_mathematicians_pi1_akt.php";s:4:"62eb";s:39:"pi1/class.tx_mathematicians_pi1_bak.php";s:4:"81d1";s:43:"pi1/class.tx_mathematicians_pi1_js_only.php";s:4:"1698";s:43:"pi1/class.tx_mathematicians_pi1_wizicon.php";s:4:"106e";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"5d17";s:16:"pi1/template.htm";s:4:"39ea";s:19:"pi1/template_en.htm";s:4:"e576";s:24:"pi1/static/editorcfg.txt";s:4:"4715";}',
+	'suggests' => array(
+	),
 );
 
 ?>
