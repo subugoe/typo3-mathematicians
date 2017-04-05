285033666c55b928d9008644da01a8c76bf01c6a
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2017-04-05 15:01:32 +0200

    Add composer support

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "mathematicians".
-#
-# Auto generated 14-06-2012 17:23
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'mathematicians',
     'description' => 'searching Oberwolfach Photo Collection and Genalogy',
@@ -31,10 +21,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'version' => '1.0.1',
     'constraints' => [
-        'depends' => [],
+        'depends' => [
+            'typo3' => '6.2.0-8.7.99',
+        ],
         'conflicts' => [],
         'suggests' => [],
     ],
-    '_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"07ba";s:10:"README.txt";s:4:"4513";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"d5d6";s:14:"ext_tables.php";s:4:"c603";s:13:"locallang.xml";s:4:"7630";s:16:"locallang_db.xml";s:4:"4dee";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:35:"pi1/class.tx_mathematicians_pi1.php";s:4:"a53b";s:39:"pi1/class.tx_mathematicians_pi1_akt.php";s:4:"62eb";s:39:"pi1/class.tx_mathematicians_pi1_bak.php";s:4:"81d1";s:43:"pi1/class.tx_mathematicians_pi1_js_only.php";s:4:"1698";s:43:"pi1/class.tx_mathematicians_pi1_wizicon.php";s:4:"106e";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"5d17";s:16:"pi1/template.htm";s:4:"39ea";s:19:"pi1/template_en.htm";s:4:"e576";s:24:"pi1/static/editorcfg.txt";s:4:"4715";}',
     'suggests' => [],
 ];
