e9ddbdb2392d7b99d9e2aad920833ee18cf15d3c
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2018-05-23 10:48:38 +0200

    Remove TCA from ext_tables.php

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['mathematicians_pi1'] = 'layout,select_key';
--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,6 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key';
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin([
     'LLL:EXT:mathematicians/locallang_db.xml:tt_content.list_type_pi1',
     $_EXTKEY.'_pi1',
