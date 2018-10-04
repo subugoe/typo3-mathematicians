c6a98728f8e06a8bd1e4cb728f53b244affc40c7
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2018-10-04 08:06:26 +0200

    Allow TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
